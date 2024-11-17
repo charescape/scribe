300bfa931acfccbeb6eb9d53b477be993f7d3cec
charescape <53265646+charescape@users.noreply.github.com>
2024-11-18 00:15:35 +0800

    Update index.blade.php

--- resources/views/themes/default/index.blade.php
+++ resources/views/themes/default/index.blade.php
@@ -19,6 +19,10 @@
     <link rel="stylesheet"
           href="https://lf6-cdn-tos.bytecdntp.com/cdn/expire-1-M/highlight.js/11.4.0/styles/github-dark.min.css">
     <script src="https://lf26-cdn-tos.bytecdntp.com/cdn/expire-1-M/highlight.js/11.4.0/highlight.min.js"></script>
+    <script src="https://lf3-cdn-tos.bytecdntp.com/cdn/expire-1-M/highlight.js/11.4.0/languages/javascript.min.js"></script>
+    <script src="https://lf6-cdn-tos.bytecdntp.com/cdn/expire-1-M/highlight.js/11.4.0/languages/php.min.js"></script>
+    <script src="https://lf26-cdn-tos.bytecdntp.com/cdn/expire-1-M/highlight.js/11.4.0/languages/python.min.js"></script>
+    <script src="https://lf6-cdn-tos.bytecdntp.com/cdn/expire-1-M/highlight.js/11.4.0/languages/bash.min.js"></script>
 
     <script src="https://lf3-cdn-tos.bytecdntp.com/cdn/expire-1-M/jets/0.14.1/jets.min.js"></script>
 
