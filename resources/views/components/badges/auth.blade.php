1424087e5f5d35c90d9df5384bf13d1a45077cb6
charescape <53265646+charescape@users.noreply.github.com>
2025-01-07 15:16:20 +0800

    Update auth.blade.php

--- resources/views/components/badges/auth.blade.php
+++ resources/views/components/badges/auth.blade.php
@@ -1,3 +1,3 @@
-@if($authenticated)@component('scribe::components.badges.base', ['colour' => "darkred", 'text' => 'requires authentication'])
+@if($authenticated)@component('scribe::components.badges.base', ['colour' => "darkred", 'text' => '需要身份认证'])
 @endcomponent
 @endif
